e378826489518b0d75a41f3d9e672979a7dcd37a
jdmwihia <jeremwihia@gmail.com>
2025-05-09 21:10:26 +0300

    Added Sign up and Login pages, no styling

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')  # Redirect to home or dashboard
+    else:
+        form = UserCreationForm()
+    return render(request, 'Accounts/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'Accounts/login.html', {'form': form})
+
+def accounthome_view(request):
+    return render(request, 'Accounts/accounthome.html')
